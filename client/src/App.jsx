d8151448b29051832ff0c44d2650f02a5fe88860
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-15 09:17:34 +0300

    Categories

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
+import Categories from "./components/Categories/Categories";
 import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
       <Header />
-      <div className="home px-6 flex justify-between">
-        <div className="categories">
-          <div>Categories</div>
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 overflow-auto max-h-[calc(100vh--112px)] pb-64">
+          <Categories />
         </div>
-        <div className="products">
+        <div className="products flex-8">
           <div>Products</div>
         </div>
         <div className="">
