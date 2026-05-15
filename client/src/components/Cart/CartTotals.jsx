6a62e3e80e4ab69a7f5401be672766fcb4cae480
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-15 10:25:13 +0300

    CartItem

--- client/src/components/Cart/CartTotals.jsx
+++ client/src/components/Cart/CartTotals.jsx
@@ -1,5 +1,9 @@
 import { Button } from "antd";
-import { ClearOutlined } from "@ant-design/icons";
+import {
+  ClearOutlined,
+  MinusCircleOutlined,
+  PlusCircleOutlined,
+} from "@ant-design/icons";
 
 const CartTotals = () => {
   return (
@@ -7,9 +11,92 @@ const CartTotals = () => {
       <h2 className="bg-blue-600 text-center py-4 text-white font-bold tracking-wide">
         Sepetteki Ürünler
       </h2>
-      <div className="cart-items">
-        <div className="cart-item">cart item</div>
-      </div>
+      <ul className="cart-items px-2 flex flex-col gap-y-3 py-2 overflow-y-auto">
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
+              alt=""
+              className="w-16 h-16 object-cover"
+            />
+            <div className="flex flex-col ml-2">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full!"
+              icon={<PlusCircleOutlined />}
+            />
+            <span>2</span>
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full!"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
+              alt=""
+              className="w-16 h-16 object-cover"
+            />
+            <div className="flex flex-col ml-2">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full!"
+              icon={<PlusCircleOutlined />}
+            />
+            <span>2</span>
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full!"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+        <li className="cart-item flex justify-between">
+          <div className="flex items-center">
+            <img
+              src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
+              alt=""
+              className="w-16 h-16 object-cover"
+            />
+            <div className="flex flex-col ml-2">
+              <b>Elma</b>
+              <span>12₺ x 2</span>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full!"
+              icon={<PlusCircleOutlined />}
+            />
+            <span>2</span>
+            <Button
+              type="primary"
+              size="small"
+              className="w-full flex items-center justify-center rounded-full!"
+              icon={<MinusCircleOutlined />}
+            />
+          </div>
+        </li>
+      </ul>
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
           <div className="flex justify-between p-2">
