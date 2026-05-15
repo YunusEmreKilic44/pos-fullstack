2162a16cb4a268d2d872c7d44a565b660d387ef9
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-15 11:14:03 +0300

    Home route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,14 @@
-import CartTotals from "./components/Cart/CartTotals";
-import Categories from "./components/Categories/Categories";
-import Header from "./components/Header/Header";
-import Products from "./components/Products/Products";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <div className="home px-6 flex flex-col md:flex-row justify-between gap-10 md:pb-0 pb-24">
-        <div className="categories overflow-auto max-h-[calc(100vh--112px)] md:pb-10  ">
-          <Categories />
-        </div>
-        <div className="products flex-8 max-h-[calc(100vh--112px)] overflow-y-auto pb-10">
-          <Products />
-        </div>
-        <div className="cart-wrapper min-w-75 md:-mr-6 md:-mt-6 border ">
-          <CartTotals />
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Routes>
     </>
   );
 };
