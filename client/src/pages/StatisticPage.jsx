59366fd8c81c07a7897aa67df1292bb95b32b8b1
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-16 12:32:48 +0300

    Ant design Charts kurulumu

--- client/src/pages/StatisticPage.jsx
+++ client/src/pages/StatisticPage.jsx
@@ -1,7 +1,57 @@
 import Header from "../components/Header/Header";
 import StatisticCard from "../components/Statistics/StatisticCard";
+import { Area, Pie } from "@ant-design/charts";
 
 const StatisticPage = () => {
+  const config = {
+    data: {
+      type: "fetch",
+      value: "https://assets.antv.antgroup.com/g2/aapl.json",
+    },
+    xField: (d) => new Date(d.date),
+    yField: "close",
+  };
+
+  const config2 = {
+    data: [
+      { type: "分类一", value: 27 },
+      { type: "分类二", value: 25 },
+      { type: "分类三", value: 18 },
+      { type: "分类四", value: 15 },
+      { type: "分类五", value: 10 },
+      { type: "其他", value: 5 },
+    ],
+    angleField: "value",
+    colorField: "type",
+    innerRadius: 0.6,
+    label: {
+      text: "value",
+      style: {
+        fontWeight: "bold",
+      },
+    },
+    legend: {
+      color: {
+        title: false,
+        position: "right",
+        rowPadding: 5,
+      },
+    },
+    annotations: [
+      {
+        type: "text",
+        style: {
+          text: "AntV\nCharts",
+          x: "50%",
+          y: "50%",
+          textAlign: "center",
+          fontSize: 40,
+          fontStyle: "bold",
+        },
+      },
+    ],
+  };
+
   return (
     <>
       <Header />
@@ -33,6 +83,14 @@ const StatisticPage = () => {
               amount={"28"}
               img="images/product.png"
             />
+          </div>
+          <div className="flex justify-between">
+            <div>
+              <Area {...config} />
+            </div>
+            <div>
+              <Pie {...config2} />
+            </div>
           </div>
         </div>
       </div>
