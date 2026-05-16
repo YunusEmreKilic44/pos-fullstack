e578dc7dde034cd681eca748f26f4694a7107c03
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-16 10:08:17 +0300

    CustomersPage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import CartPage from "./pages/CartPage";
 import BillsPage from "./pages/BillsPage";
+import CustomersPage from "./pages/CustomersPage";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/cart" element={<CartPage />} />
         <Route path="/bills" element={<BillsPage />} />
+        <Route path="/customers" element={<CustomersPage />} />
       </Routes>
     </>
   );
