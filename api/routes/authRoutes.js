f54163fc1ac808d6713326262072c09a344864ae
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-24 20:46:46 +0300

    bcryptjs

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,12 +1,23 @@
 const User = require("../models/User.js");
 const express = require("express");
+const bcrypt = require("bcryptjs");
 
 const router = express.Router();
 
 //! register
 router.post("/register", async (req, res) => {
   try {
-    const newUser = new User(req.body);
+    const { username, email, password } = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
     await newUser.save();
     return res.status(200).json("A new User created successfully.");
   } catch (error) {
