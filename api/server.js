b62f81981f3177d5d535d54b8069d8502b223d54
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-24 10:50:45 +0300

    mongoose kurulumu

--- api/server.js
+++ api/server.js
@@ -1,13 +1,27 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 const port = 5000;
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to mongoDB");
+  } catch (error) {
+    throw error;
+  }
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(port, () => {
+  connect();
   console.log(`Server is running on port ${port}...`);
 });
