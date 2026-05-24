1ee8b81c7ff079365a32cc7e32b0f88d3daa2624
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-24 18:08:07 +0300

    UserSchema

---
+++ api/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    verify: {
+      type: Boolean,
+    },
+  },
+  {
+    timestamps: true,
+    required: true,
+  },
+);
+
+const User = mongoose.model("users", UserSchema);
+
+module.exports = User;
