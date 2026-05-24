d08ef3cbc7daad84e669b34dd0c1b6e7444e02ce
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-24 19:03:36 +0300

    get-post-put-delete categories

--- api/routes/categoryRoutes.js
+++ api/routes/categoryRoutes.js
@@ -3,11 +3,44 @@ const express = require("express");
 
 const router = express.Router();
 
+//! get all category
+router.get("/get-all", async (req, res) => {
+  try {
+    const categories = await Category.find();
+    return res.status(200).json(categories);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//!create
 router.post("/add-category", async (req, res) => {
   try {
     const newCategory = new Category(req.body);
     await newCategory.save();
     return res.status(200).json("Item added successfully");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//!Update
+router.put("/update-category", async (req, res) => {
+  try {
+    await Category.findOneAndUpdate({ _id: req.body.categoryId }, req.body, {
+      new: true,
+    });
+    return res.status(200).json("Item updated successfully.");
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+//!delete
+router.delete("/delete-category", async (req, res) => {
+  try {
+    await Category.findOneAndDelete({ _id: req.body.categoryId });
+    return res.status(200).json("Item deleted successfully.");
   } catch (error) {
     res.status(400).json(error);
   }
