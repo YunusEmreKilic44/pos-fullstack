cf067d14bce1861a013cfe9119cddda4d1fa0d27
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-25 12:14:38 +0300

    Kategori post işlemi

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,30 +1,80 @@
+import { useState } from "react";
+import "./style.css";
+import { PlusOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Modal, message } from "antd";
+
 const Categories = () => {
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    try {
+      fetch("http://localhost:5000/api/categories/add-category", {
+        method: "POST",
+        body: JSON.stringify(values),
+        headers: { "Content-type": "application/json; charset=UTF-8" },
+      });
+      message.success("Kategori başarıyla eklendi.");
+      form.resetFields();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <ul className="flex gap-4 md:flex-col text-lg">
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center">
+      <li className="category-item">
         <span>Tümü</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>Yiyecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
+      <li className="category-item">
         <span>İçecek</span>
       </li>
-      <li className="bg-green-700 px-6 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center min-w-[145px]">
-        <span>İçecek</span>
+
+      <li
+        className="category-item bg-purple-800! hover:opacity-90"
+        onClick={() => setIsAddModalOpen(true)}
+      >
+        <PlusOutlined classID="md:text-2xl" />
       </li>
+      <Modal
+        title="Yeni Kategori Ekle"
+        open={isAddModalOpen}
+        closable={{ "aria-label": "Custom Close Button" }}
+        onCancel={() => setIsAddModalOpen(false)}
+        footer={false}
+      >
+        <Form layout="vertical" onFinish={onFinish} form={form}>
+          <Form.Item
+            name={"title"}
+            label="Kategori Ekle"
+            rules={[
+              { required: true, message: "Kategori alanı boş bırakılamaz!" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item className="flex justify-end mb-0">
+            <Button type="primary" htmlType="submit">
+              Oluştur
+            </Button>
+          </Form.Item>
+        </Form>
+      </Modal>
     </ul>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,10 +8,10 @@ const HomePage = () => {
     <div>
       <Header />
       <div className="home px-6 flex flex-col md:flex-row justify-between gap-10 md:pb-0 pb-24">
-        <div className="categories overflow-auto max-h-[calc(100vh--112px)] md:pb-10  ">
+        <div className="categories overflow-auto max-h-[calc(100vh-112px)] md:pb-10">
           <Categories />
         </div>
-        <div className="products flex-8 max-h-[calc(100vh--112px)] overflow-y-auto pb-10">
+        <div className="products flex-8 max-h-[calc(100vh-112px)] overflow-y-auto pb-10">
           <Products />
         </div>
         <div className="cart-wrapper min-w-75 md:-mr-6 md:-mt-6 border ">
