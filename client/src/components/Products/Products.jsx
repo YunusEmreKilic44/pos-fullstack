8ef3f20ca444efd1a97f352429fa65fd75288cbe
Yunus Emre Kılıç <yunusemrkilic44@gmail.com>
2026-05-25 14:42:52 +0300

    products get işlemi

--- client/src/components/Products/Products.jsx
+++ client/src/components/Products/Products.jsx
@@ -1,136 +1,42 @@
+import { useEffect, useState } from "react";
+
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await fetch("http://localhost:5000/api/products/get-all");
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getProducts();
+  }, []);
+
   return (
     <div className="product-wrapper grid gap-4 grid-cols-[repeat(auto-fill,minmax(150px,1fr))]">
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
+      {products.map((item) => (
+        <div
+          key={item._id}
+          className="product-item border hover:shadow-lg cursor-pointer transition-all select-none"
+        >
+          <div className="product-image">
+            <img
+              src={item.img}
+              alt={item.title}
+              className="h-28 object-cover w-full border-b"
+            />
+          </div>
+          <div className="product-info flex flex-col p-3">
+            <span className="font-bold">{item.title}</span>
+            <span>{item.price}₺</span>
+          </div>
         </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
-      <div className="product-item border hover:shadow-lg cursor-pointer transition-all select-none">
-        <div className="product-image">
-          <img
-            src="https://ichef.bbci.co.uk/ace/ws/640/cpsprodpb/8e46/live/82d73790-5271-11f0-a2ff-17a82c2e8bc4.jpg.webp"
-            alt=""
-            className="h-28 object-cover w-full border-b"
-          />
-        </div>
-        <div className="product-info flex flex-col p-3">
-          <span className="font-bold">Elma</span>
-          <span>12₺</span>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
